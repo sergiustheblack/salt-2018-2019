6f7cf5100bedbb6c377252e4e7c076ef72772271
sergiustheblack <sergius@goes.black>
2020-05-03 09:49:22 +0300

    reload2

--- reload-iptables.sh
+++ reload-iptables.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo iptables -S | grep -qi docker || sudo service iptables restart
+sudo /usr/sbin/iptables -S | grep -qi docker && echo "$(hostname) Docker found" || ( sudo systemctl restart iptables && echo "$(hostname) iptables restarted")
+
